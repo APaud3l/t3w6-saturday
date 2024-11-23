0713e1846b4f717b3639488158f40c6bdb07ec88
APaud3l <aamod.paudel@coderacademy.edu.au>
2024-11-23 12:16:39 +1100

    feat: Nested route

--- src/App_ComponentRouter.jsx
+++ src/App_ComponentRouter.jsx
@@ -3,7 +3,9 @@ import './styles/App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import TerminalAppProject from './pages/TerminalAppProject';
 import Navbar from './components/Navbar';
+import ProjectLayout from './pages/_templates/ProjectLayout';
 
 function App() {
   return (
@@ -16,6 +18,10 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/projects" element={<ProjectLayout />}>
+            <Route path='/projects/terminalApp' element={<TerminalAppProject />} />
+            <Route path='terminalApp' element={<TerminalAppProject />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
